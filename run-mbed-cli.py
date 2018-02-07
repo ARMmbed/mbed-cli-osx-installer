2bf93ccf1042e31548ee3cd51e3c1b116a2d6ab5
Blackstone Engineering <austin.blackstone@arm.com>
2018-02-06 22:15:05 -0600

    python calls run-mbed-cli.sh, which open a terminal

--- run-mbed-cli.py
+++ run-mbed-cli.py
@@ -1,9 +1,11 @@
-
-# import os
-# os.spawnl(os.P_WAIT, 'open --fresh -a open /Applications/Utilities/Terminal.app/Contents/MacOS/Terminal')
-
 import subprocess
 
+# open Terminal
+# subprocess.call(
+#     ["/usr/bin/open", "-W", "-n", "--fresh","-a", "/Applications/Utilities/Terminal.app"]
+#     )
+
+# call shell script that call terminal
 subprocess.call(
-    ["/usr/bin/open", "-W", "-n", "--fresh","-a", "/Applications/Utilities/Terminal.app"]
+    ["/bin/sh","/Users/ausbla01/mbed/mbed-cli-osx-installer/run-mbed-cli.sh"]
     )
---
+++ run-mbed-cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal
+
+# CWD=$PWD
+export TESTY_MC_TESTFACE="TESTy_MC_TEST"
+echo "hello world"
+open -W -n --fresh -a /Applications/Utilities/Terminal.app 
+echo "terminal should be running now...."
